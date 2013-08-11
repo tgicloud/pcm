e20f21e610dc931131a970485b4fae5a9056d435
Sean Smith <dremoor@gmail.com>
2013-08-11 16:13:16 -0400

    more proto crap

---
+++ prototypes/crapity-crap/Navigator2.js
@@ -0,0 +1,19 @@
+/**
+ * pcm
+ * Navigator2.js
+ */
+
+var pcm = {};
+pcm.lastPanelShown = "homePanel";
+pcm.lastActiveNav = "";
+// navBtnMatchPlay class="active"
+function command(cmd) {
+  if (pcm.lastActiveNav)
+    $('#' + pcm.lastActiveNav).removeClass('active');
+  $("#navCollapse").collapse('hide');
+  $('#' + pcm.lastPanelShown).hide();
+  pcm.lastPanelShown = cmd + "Panel";
+  pcm.lastActiveNav = cmd + "NavBtn";
+  $('#' + pcm.lastActiveNav).addClass('active');
+  $('#' + pcm.lastPanelShown).show();
+}
