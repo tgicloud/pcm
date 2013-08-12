b25f4e2b98216a4a87f4b61d63e4d5a9d205c373
Sean Smith <dremoor@gmail.com>
2013-08-12 11:34:24 -0400

    more proto crap

--- prototypes/crapity-crap/Navigator2.js
+++ prototypes/crapity-crap/Navigator2.js
@@ -5,15 +5,15 @@
 
 var pcm = {};
 pcm.lastPanelShown = "homePanel";
-pcm.lastActiveNav = "";
+//pcm.lastActiveNav = "";
 // navBtnMatchPlay class="active"
 function command(cmd) {
-  if (pcm.lastActiveNav)
-    $('#' + pcm.lastActiveNav).removeClass('active');
-  $("#navCollapse").collapse('hide');
+//  if (pcm.lastActiveNav)
+//    $('#' + pcm.lastActiveNav).removeClass('active');
+//  $("#navCollapse").collapse('hide');
   $('#' + pcm.lastPanelShown).hide();
   pcm.lastPanelShown = cmd + "Panel";
-  pcm.lastActiveNav = cmd + "NavBtn";
-  $('#' + pcm.lastActiveNav).addClass('active');
+//  pcm.lastActiveNav = cmd + "NavBtn";
+//  $('#' + pcm.lastActiveNav).addClass('active');
   $('#' + pcm.lastPanelShown).show();
 }
